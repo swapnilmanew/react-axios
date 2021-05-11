c32f7a854fb852592242250ec9d0576ea039c0d8
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-11 07:27:33 +0000

    asdf

--- axios-crud/src/App.js
+++ axios-crud/src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Inde} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ axios-crud/src/components/Index.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                </div>
+                <div className="col-12 m-auto col-md-7">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="col-12 m-auto col-md-5"></div>
+            </div>
+        </>
+    )
+}
+
+export default Index
