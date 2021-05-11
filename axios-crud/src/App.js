b8032d39c3d861e9b5deb11db66d8f2ab7dc7b4a
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-11 07:44:35 +0000

    mui

--- axios-crud/src/App.js
+++ axios-crud/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Index from "./components/Index";
+import Add from "./components/Add";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
         </Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/add" component={Add} />
+        <Route exact path="/" component={Index} />
+        <Route exact path="/add" component={Add} />
       </BrowserRouter>
     </>
   );
--- axios-crud/src/components/Index.jsx
+++ axios-crud/src/components/Index.jsx
@@ -3,57 +3,7 @@ import React from 'react'
 const Index = () => {
     return (
         <>
-            <div className="container">
-                <div className="row">
-                </div>
-                <div className="col-12 m-auto col-md-7">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="col-12 m-auto col-md-5"><form>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-                </div>
-          
-            </div>
-        </>
+            </>
     )
 }
 
